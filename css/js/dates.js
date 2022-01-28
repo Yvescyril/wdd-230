adf68bd7868948e35f836d092e3313eca8b2a904
Yvescyril <cyrilyves61@gmail.com>
2022-01-27 20:50:06 -0500

    updated files

---
+++ css/js/dates.js
@@ -0,0 +1,3 @@
+//Test Change
+const options = {weekday:"numeric", month: "long", year:"numeric"};
+document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en us", Options);
---
+++ js/index.js
@@ -0,0 +1,5 @@
+const year = document.getElementById("year");
+let last_updated = document.getElementById("last_updated");
+
+year.textContent = new Date().getFullYear();
+// last_updated = new Date(document.lastModified);
