c6896a374741caa9a6a7c529063db65895f78317
Yvescyril <cyrilyves61@gmail.com>
2022-01-28 19:51:56 -0500

    ok

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const last_updated = document.querySelector("#last_updated");
-//let last_updated = document.getElementById("last_updated");
+//const last_updated = document.querySelector("#last_updated");
+let last_updated = document.getElementById("last_updated");
 
 //year.textContent = new Date().getFullYear();
 last_updated.textContent = document.lastModified;
