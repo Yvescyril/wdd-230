1bee8ae49e3f1ddd37d1e41a39b1172d67657941
Yvescyril <cyrilyves61@gmail.com>
2022-03-27 01:12:07 -0400

    chamber of commerce final

---
+++ chamber/js/date.js
@@ -0,0 +1,11 @@
+const datefield = document.querySelector("small");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
--- css/js/dates.js
+++
@@ -1,7 +0,0 @@
-//Test Change
-document.getElementById('currentDate').textContent = `${
-    weekday[d.getDay()]
-  }, ${d.getDate()} ${month[d.getMonth()]} ${d.getFullYear()}`;
-  
-  document.getElementById('lastmod').textContent = document.lastModified;
-  
